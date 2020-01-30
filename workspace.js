6f0fa2ea5fcf2c6f8b3a0fc7639b6a9d6717f2db
Brady Liechty <berrybloxinator@gmail.com>
2020-01-29 18:23:25 -0700

    Fully functional

--- workspace.js
+++ workspace.js
@@ -5,9 +5,24 @@ const io = require("socket.io")(http);
 let port = process.env.PORT || 8080;
 let userCount = 0;
 let users = [];
+let timeout;
 
 function removeUser(id) {
     users = users.filter(user => user.id !== id);
+}
+
+function printOnlineUsers() {
+    for (let user of users) {
+        user.emit("online users", {
+            id: user.id,
+            users: users.map(user => {
+                return {
+                    userName: user.userName,
+                    id: user.id
+                };
+            })
+        });
+    }
 }
 
 app.use(express.static(__dirname + "/public"));
@@ -17,15 +32,7 @@ io.on("connection", socket => {
     socket.userName = `user${userCount}`;
     users.push(socket);
 
-    socket.emit("online users", {
-        id: socket.id,
-        users: users.map(user => {
-            return {
-                userName: user.userName,
-                id: user.id
-            };
-        })
-    });
+    printOnlineUsers();
 
     socket.broadcast.emit("chat message", `${socket.userName} connected`);
 
@@ -33,31 +40,49 @@ io.on("connection", socket => {
         socket.emit("connected", socket.userName);
     });
 
+    socket.on("change name", userName => {
+        if (users.some(user => user.userName === userName)) {
+            socket.emit("change name error", `You or someone else already has the name ${userName}`);
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                socket.emit("change name error", "");
+            }, 3000);
+        } else {
+            socket.userName = userName;
+            printOnlineUsers();
+        }
+    });
+
     socket.on("chat message", msg => {
-        console.log(`message from ${msg.name}: ${msg.msg}`);
-        socket.broadcast.emit("chat message", {
-            msg: msg.msg,
-            name: msg.name
-        });
+        console.log(`message from ${msg.name} to ${msg.toWho}: ${msg.msg}`);
+        if (msg.toWho === "all") {
+            socket.broadcast.emit("chat message", {
+                msg: msg.msg,
+                name: msg.name
+            });
+        } else {
+            const user = users.filter(user => user.userName === msg.toWho)[0];
+            user.emit("chat message", `*Whisper from ${socket.userName}: ${msg.msg}*`);
+        }
     });
 
     socket.on("user-typing", msg => {
-        console.log(msg.msg);
-        socket.userName = msg.userName;
-        socket.broadcast.emit("user-typing", msg.msg);
+        if (msg.msg === "yes") {
+            if (msg.toWho === "all") {
+                socket.broadcast.emit("user-typing", `${socket.userName} is typing...`);
+            } else {
+                const user = users.filter(user => user.userName === msg.toWho)[0];
+                user.emit("user-typing", `${socket.userName} is typing...`);
+            }
+        } else if (msg.msg === "no") {
+            socket.broadcast.emit("user-typing", "");
+        }
     });
 
     socket.on("disconnect", () => {
         removeUser(socket.id);
-        socket.broadcast.emit("online users", {
-            users: users.map(user => {
-                return {
-                    userName: user.userName,
-                    id: user.id
-                };
-            })
-        });
-        console.log("socket disconnected");
+        printOnlineUsers();
+        socket.broadcast.emit();
     });
 });
 
